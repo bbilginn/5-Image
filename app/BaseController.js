af6c8a86473904fb5fe05ebd77fbabef2cd2076b
bbilginn <bbilginn@gmail.com>
2014-01-31 12:00:31 +0200

    İlk resmin aktif olarak gelmesi.

--- app/BaseController.js
+++ app/BaseController.js
@@ -1,15 +1,18 @@
 ﻿function BaseController($scope, $http) {
 
     // Resimlerin servisten geldiğini simüle ediyoruz.
-    var Images = new Array();
-    Images.push({ _Id: 1, Name: "Minik Panda", ImageUrl: "img/1.jpg", ThumbImageUrl: "img/_thumb/1.jpg" });
-    Images.push({ _Id: 2, Name: "Sonsuz Deniz", ImageUrl: "img/2.jpg", ThumbImageUrl: "img/_thumb/2.jpg" });
-    Images.push({ _Id: 3, Name: "Parmak İzi", ImageUrl: "img/3.jpg", ThumbImageUrl: "img/_thumb/3.jpg" });
-    Images.push({ _Id: 4, Name: "Astronotlar", ImageUrl: "img/4.jpg", ThumbImageUrl: "img/_thumb/4.jpg" });
-    Images.push({ _Id: 5, Name: "Kelebek", ImageUrl: "img/5.jpg", ThumbImageUrl: "img/_thumb/5.jpg" });
-    $scope.ImageList = Images;
+    $scope.ImageList = [
+        { _Id: 1, Name: "Minik Panda", ImageUrl: "img/1.jpg", ThumbImageUrl: "img/_thumb/1.jpg" },
+        { _Id: 2, Name: "Sonsuz Deniz", ImageUrl: "img/2.jpg", ThumbImageUrl: "img/_thumb/2.jpg" },
+        { _Id: 3, Name: "Parmak İzi", ImageUrl: "img/3.jpg", ThumbImageUrl: "img/_thumb/3.jpg" },
+        { _Id: 4, Name: "Astronotlar", ImageUrl: "img/4.jpg", ThumbImageUrl: "img/_thumb/4.jpg" },
+        { _Id: 5, Name: "Kelebek", ImageUrl: "img/5.jpg", ThumbImageUrl: "img/_thumb/5.jpg" }
+    ];
 
-    // Küçük resim tıklandığında değiştirmekle yükümlü event
+    // İlk resim sabit gelir.
+    $scope.CurrentImage = $scope.ImageList[0];
+
+    // Küçük resim tıklandığında değiştirmekle yükümlü event.
     $scope.ChangeImage = function () {
 
     }
